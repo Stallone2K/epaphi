09bc496cf72edd0db4fe7b21fcfbc4b1b63ab0f5
Stallone2K <stallonefernandess@gmail.com>
2025-01-25 00:59:51 +0000

    Init

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import SideBar from '@/components/sidebar'
+
+type Props = { children: React.ReactNode }
+
+const Layout = (props: Props) => {
+	return (
+		<div className="flex overflow-hidden h-screen">
+			<SideBar />
+			<div className="w-full">
+				{/* <InfoBar /> */}
+				{props.children}
+			</div>
+		</div>
+	)
+}
+
+export default Layout
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import NavBar from "../components/global/NavBar";
+
+export default function Home() {
+  return (
+    <div className="">
+      <NavBar></NavBar>
+
+    </div >
+  );
+}
