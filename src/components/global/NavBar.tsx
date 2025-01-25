d6a1deb509e0cae108117ad1601eee7e08a27c17
Stallone2K <stallonefernandess@gmail.com>
2025-01-25 01:48:30 +0000

    Init

--- src/components/global/NavBar.tsx
+++ src/components/global/NavBar.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link"
 import { MenuIcon } from 'lucide-react'
+import Image from 'next/image'
 type Props = {}
 
 const NavBar = (props: Props) => {
 	return (
 		<header className="
-fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
+fixed right-0 left-0 top-0 py-2 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
 			<aside className="flex items-center gap-[2px]">
-				<p className="text-3xl font-bold">επαφή</p>
+				<p className="text-3xl font-bold">
+					<Image src={'/Logo.png'} width={100} height={100} alt="Epaphi Automations">
+					</Image>
+				</p>
 			</aside>
 			<nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
 				<ul className="flex items-center gap-4 list-none">
